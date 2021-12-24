29b62383f5100e71b797ecff631593347d2152b9
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2021-12-24 19:42:20 +0100

    first solution with callback

--- index.js
+++ index.js
@@ -8,5 +8,33 @@ fs.readdir(process.cwd(),(err,filenames)=>{
         console.log(err);
         return
     }
-    console.log(filenames)
-});
+
+    const allStats = Array(filenames.length).fill(null) // create an empty array with the same number of files
+    
+    for(let filename of filenames){
+        fs.lstat(filename,(err,stats) => {
+
+        const index = filenames.indexOf(filename) // get the index of the current iteration
+        
+
+        if (err){
+            console.log(err)
+        }
+
+        allStats[index] = stats; // store the value of the returned stats value using the same index position in the new allStats array
+        
+         const ready = allStats.every((el)=>{
+            return el;
+
+        }); //evaluates to true there are no more null values in the allStats array 
+        // on every loop ready function is evaluating the all stats to ensure there are no more fallsy values
+
+        if (ready){
+            allStats.forEach((stats, index)=>{
+                console.log(filenames[index],stats.isFile())
+            });
+        }
+    }); //end of lstat
+
+    }; //end of for loop in the directory.
+}); //end of readdir
